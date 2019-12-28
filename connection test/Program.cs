22a762ad2cf202a68a5c2bc89a96f276f9760dd1
xuri <T0Bl4S.durst@gmail.com>
2019-12-28 16:21:03 +0100

    testing components

---
+++ connection test/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace connection_test {
+    class Program {
+        static void Main(string[] args) {
+            var s = new Socket( SocketType.Stream, ProtocolType.Tcp );
+            s.Connect( IPAddress.Parse( "127.0.0.1" ), 9900 );
+            new Thread( () => {
+                int i = 10;
+                while ( s.Connected ) {
+                    if ( s.Available > 0 ) {
+                        var size = s.Available < 1024 ? s.Available : 1024;
+                        s.Receive( new byte[size] );
+                        Console.WriteLine( "[+] Received " + size );
+                    }
+
+                    s.Send( Enumerable.Repeat( (byte) 1, 1024 ).ToArray() );
+                    Console.WriteLine( "[-] Send 1024 " );
+                    Thread.Sleep( 100 );
+                }
+            } ).Start();
+        }
+    }
+}
---
+++ replay connection/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace replay_connection {
+    class Program {
+        static void Main(string[] args) {
+            var s = new Socket( SocketType.Stream, ProtocolType.Tcp );
+            s.Bind( new IPEndPoint( IPAddress.Any, 2222 ) );
+            s.Listen( 1000 );
+            Console.WriteLine( "Listen.." );
+            while ( true ) {
+                var c = s.Accept();
+                Console.WriteLine( "got one..." );
+                new Thread( () => {
+                    while ( c.Connected ) {
+                        if ( c.Available > 0 ) {
+                            var size = c.Available < 1024 ? c.Available : 1024;
+                            var bu   = new byte[size];
+                            c.Receive( bu );
+                            c.Send( bu );
+                            Console.WriteLine( "[+] Received/Send " + size );
+                        }
+                    }
+                } ).Start();
+            }
+        }
+    }
+}
